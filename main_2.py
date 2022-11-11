e63cdfd704fdb716dbc75bf0ff81a6bea5ce903a
tinatartina <crusco.martina.maria@iisgalvanimi.edu.it>
2022-11-11 10:34:22 +0000

    v1.2

---
+++ main_2.py
@@ -0,0 +1,23 @@
+ #stampa la lista 
+ thislist = ["apple", "banana", "cherry", "apple", "cherry"]
+print(thislist)
+
+#Stampa il numero di elementi nell'elenco
+thislist = ["apple", "banana", "cherry"]
+print(len(thislist))
+
+#stampa le tre liste 
+list1 = ["apple", "banana", "cherry"]
+list2 = [1, 5, 7, 9, 3]
+list3 = [True, False, False]
+
+#stampi tutto quello che c'è nella lista che sia un nome o che sia un numero 
+list1 = ["abc", 34, True, 40, "male"]
+
+#stampa mylist perchè c'è il type
+mylist = ["apple", "banana", "cherry"]
+print(type(mylist))
+
+#
+thislist = ["apple", "banana", "cherry"]
+print(thislist[1])
